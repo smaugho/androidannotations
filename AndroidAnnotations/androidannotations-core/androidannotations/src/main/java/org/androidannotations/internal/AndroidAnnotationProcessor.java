f7cc768342965b68f99ad48d037356c6ee303743
Adrián Rivero <arivero.smaugh@gmail.com>
2017-03-22 10:53:29 +0100

    Fix merging bug.

--- AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/internal/AndroidAnnotationProcessor.java
+++ AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/internal/AndroidAnnotationProcessor.java
@@ -325,8 +325,6 @@ public class AndroidAnnotationProcessor extends AbstractProcessor {
 
 	protected AnnotationElements validateAnnotations(AnnotationElements extractedModel, AnnotationElementsHolder validatingHolder) {
 		timeStats.start("Validate Annotations"); 
-	private AnnotationElements validateAnnotations(AnnotationElements extractedModel, AnnotationElementsHolder validatingHolder) {
-		timeStats.start("Validate Annotations");
 		ModelValidator modelValidator = new ModelValidator(androidAnnotationsEnv);
 		AnnotationElements validatedAnnotations = modelValidator.validate(extractedModel, validatingHolder);
 		timeStats.stop("Validate Annotations");
